49a6880e24c6782fb88f5056041a5a59fcf7879b
赵俊 <mblq527@163.com>
2018-11-13 21:37:49 +0800

    商品销售页+

--- src/views/Member/Cost/Cost.vue
+++ src/views/Member/Cost/Cost.vue
@@ -27,15 +27,15 @@
 									<h2 class="goods-name">{{food.name}}</h2>
 									<p class="goods-desc">{{food.description}}</p>
 									<div class="goods-extra">
-										<span>
+										<span class="goods-count">
 											月售{{food.sellCount}}份
 										</span>
 										<span>
 											好评率{{food.rating}}%
 										</span>
-										<div class="price">
-											<span>￥{{food.price}}</span>
-											<span></span>
+										<div class="goods-price">
+											<span class="goods-price-now">￥{{food.price}}</span>
+											<span class="goods-price-old"></span>
 										</div>
 									</div>
 								</div>
@@ -49,6 +49,7 @@
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 import Back from '@/components/Back/Back.vue'
 import goods from '@static/goods.json'
 
@@ -63,6 +64,11 @@ export default {
 			goods: goods.goods,
 		}
 	},
+	methods: {
+		initScroll() {
+
+		}
+	}
 }
 </script>
 
@@ -84,7 +90,7 @@ export default {
 				display: table;
 				height: 54px;
 				line-height: 14px;
-				padding: 0 12px;
+    			margin: 12px;
 				.border();
 				.goods-text {
 					display: table-cell;
@@ -98,6 +104,67 @@ export default {
 		.goods-wrapper {
 			flex: 1;
 			background: #fff;
+			.goods-title {
+				padding-left: 14px;
+				height: 26px;
+				line-height: 26px;
+				border-left: 2px solid #d9dde1;
+				font-size: 12px;
+				color: rgb(147, 153, 159);
+				background: #f3f5f7
+			}
+			.goods-item {
+				display: flex;
+				margin: 18px;
+				padding-bottom: 18px;
+				.border();
+				&:last-child {
+					.border-none();
+					margin-bottom: 0px;
+				}
+				.goods-icon {
+					flex: 0 0 57px;
+					margin-right: 10px;
+				}
+				.goods-content {
+					flex: 1;
+					.goods-name {
+						margin: 2px 0 8px 0;
+						height: 14px;
+						line-height: 14px;
+						font-size: 14px;
+						font-weight: 600;
+						color: rgb(7, 17, 27);
+					}
+					.goods-desc, .goods-extra {
+						line-height: 10px;
+						font-size: 10px;
+						color: rgb(147, 157, 159);
+					}
+					.goods-desc {
+						margin-bottom: 8px;
+					}
+					.goods-extra {
+						& .goods-count {
+							margin-right: 12px;
+						}
+					}
+					.goods-price {
+						font-weight: 700;
+						line-height: 24px;
+						.goods-price-now {
+							margin-right: 8px;
+							font-size: 14px;
+							color: rgb(240, 20, 20);
+						}
+						.goods-price-old {
+							text-decoration: line-through;
+							font-size: 10px;
+							color: rgb(147, 153, 159);
+						}
+					}
+				}
+			}
 		}
 	}
 }
