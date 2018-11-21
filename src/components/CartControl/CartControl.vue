1c41df5f4dc3cce6a0c03e58530a53202a0ceef4
赵俊 <mblq527@163.com>
2018-11-21 22:07:06 +0800

    购物控制组件

--- src/components/CartControl/CartControl.vue
+++ src/components/CartControl/CartControl.vue
@@ -1,16 +1,26 @@
 <template>
 	<div class="cart-control">
-		<div class="cc-decrease" v-show="!good.count">
-			<i class="iconfont icon-jianhao cc-icon"/>
-		</div>
+		<transition name="move">
+			<div
+				class="cc-decrease"
+				v-show="!!good.count"
+				@click="decreaseCart($event)"
+			>
+				<div class="cc-inner">
+					<i class="iconfont icon-jianhao cc-icon"/>
+				</div>
+			</div>
+		</transition>
 		<div class="cc-count" v-show="!!good.count">{{good.count}}</div>
-		<div class="cc-add">
+		<div class="cc-add" @click="addCart($event)">
 			<i class="iconfont icon-jiahao1 cc-icon"/>
 		</div>
 	</div>
 </template>
 
 <script>
+import Vue from 'vue'
+
 export default {
 	name: 'cart-control',
 	props: {
@@ -24,14 +34,34 @@ export default {
 	data() {
 		return {}
 	},
-	created() {
-		log(this.good)
+	methods: {
+		addCart(event) {
+			// 非better-scroll派发的事件（浏览器原生事件）
+			if (!event._constructed) {
+				return
+			}
+			if (!this.good.count) {
+				Vue.set(this.good, 'count', 1)
+			} else {
+				this.good.count++
+			}
+		},
+		decreaseCart(event) {
+			// 非better-scroll派发的事件（浏览器原生事件）
+			if (!event._constructed) {
+				return
+			}
+			if (this.good.count) {
+				this.good.count--
+			}
+		}
 	}
 }
 </script>
 
 <style lang="less">
 @import '../../../static/css/variable';
+@transition: all 0.4s linear;
 .cart-control {
 	font-size: 0;
 	.cc-icon {
@@ -44,16 +74,37 @@ export default {
 	.cc-decrease {
 		display: inline-block;
 		padding: 6px;
+		opacity: 1;
+		transition: @transition;
+		transform: translate3d(0, 0, 0);
+		.cc-inner {
+			transition: @transition;
+			transform: rotate(0deg);
+		}
+		&.move-enter, &.move-leave-active {
+			opacity: 0;
+			transform: translate3d(24px, 0, 0);
+			.cc-inner {
+				transform: rotate(180deg);
+			}
+		}
 		.cc-icon {
 			background: @active-bg-color;
 			color: @active-text-color;
-			border: 1px @active-text-color solid;
+			border: 2px @active-text-color solid;
 			box-sizing: border-box;
-			padding: 2px;
+			padding: 1px;
 		}
 	}
 	.cc-count {
 		display: inline-block;
+		vertical-align: top;
+		width: 12px;
+		padding-top: 4px;
+		line-height: 24px;
+		text-align: center;
+		font-size: 12px;
+		color: rgb(147, 153, 159);
 	}
 	.cc-add {
 		display: inline-block;
