186c99ddb211a413acd1c58a963cee8a75f9d244
赵俊 <mblq527@163.com>
2018-12-17 21:55:30 +0800

    商品详情页++

--- src/components/Good/Good.vue
+++ src/components/Good/Good.vue
@@ -1,31 +1,38 @@
 <template>
 	<transition name="move">
 		<div v-show="show_flag" class="good" ref="good">
-			<div class="good-content">
-				<div class="good-image-header" @click="hide">
-					<div class="back">
-						<i class="iconfont icon-guanbi1"/>
+			<div>
+				<div class="good-content">
+					<div class="good-image-header" @click="hide">
+						<div class="back">
+							<i class="iconfont icon-guanbi1"/>
+						</div>
+						<img :src="good.image">
 					</div>
-					<img :src="good.image">
-				</div>
-				<div class="good-body">
-					<h1 class="good-title">{{good.name}}</h1>
-					<div class="good-detail">
-						<span class="good-sell-count">月售{{good.sellCount}}份</span>
-						<span class="good-rating">好评率{{good.rating}}%</span>
+					<div class="good-body">
+						<h1 class="good-title">{{good.name}}</h1>
+						<div class="good-detail">
+							<span class="good-sell-count">月售{{good.sellCount}}份</span>
+							<span class="good-rating">好评率{{good.rating}}%</span>
+						</div>
+						<div class="good-price">
+							<span class="good-price-now">￥{{good.price}}</span>
+							<span
+								class="good-price-old"
+								v-show="good.oldPrice"
+							>￥{{good.oldPrice}}</span>
+						</div>
 					</div>
-					<div class="good-price">
-						<span class="good-price-now">￥{{good.price}}</span>
-						<span
-							class="good-price-old"
-							v-show="good.oldPrice"
-						>￥{{good.oldPrice}}</span>
+					<div class="cartcontrol-wrapper">
+						<CartControl :good="good"/>
 					</div>
+					<div class="good-buy" @click="addFirst($event)" v-show="!good.count">加入购物车</div>
 				</div>
-				<div class="cartcontrol-wrapper">
-					<CartControl :good="good"/>
+				<Split v-show="good.info" />
+				<div class="good-info">
+					<h1 class="good-title">商品信息</h1>
+					<p class="good-text">{{good.info}}</p>
 				</div>
-				<div class="good-buy" @click="addFirst($event)" v-show="!good.count">加入购物车</div>
 			</div>
 		</div>
 	</transition>
@@ -35,11 +42,13 @@
 import Vue from 'Vue'
 import BScroll from 'better-scroll'
 import CartControl from '@/components/CartControl/CartControl'
+import Split from '@/components/Split/Split'
 
 export default {
 	name: 'good',
 	components: {
 		CartControl,
+		Split,
 	},
 	props: {
 		good: {
@@ -57,7 +66,10 @@ export default {
 			this.show_flag = !this.show_flag
 			this.$nextTick(() => {
 				if (!this.scroll) {
+					log(this.$refs.good)
 					this.scroll = new BScroll(this.$refs.good, { click: true })
+				} else {
+					this.scroll.refresh()
 				}
 			})
 		},
@@ -83,91 +95,108 @@ export default {
 	top: 0;
 	bottom: 48px;
 	width: 100%;
-	background: #ccc;
+	background: #fff;
 	z-index: 10;
 	transition: all 0.2s linear;
 	&.move-enter, &.move-leave-active {
 		transform: translate3d(0, -100%, 0);
 	}
-	.good-image-header {
+	.good-content {
 		position: relative;
-		padding-top: 100%;
-		height: 0;
-		width: 100%;
-		.back {
-			position: absolute;
-			top: 10px;
-			left: 10px;
-			color: @primary-color;
-			z-index: 1;
-			.iconfont {
-				font-size: 25px;
+		.good-image-header {
+			position: relative;
+			padding-top: 100%;
+			height: 0;
+			width: 100%;
+			.back {
+				position: absolute;
+				top: 10px;
+				left: 10px;
+				color: @primary-color;
+				z-index: 1;
+				.iconfont {
+					font-size: 25px;
+				}
+			}
+			img {
+				position: absolute;
+				top: 0;
+				left: 0;
+				width: 100%;
+				height: 100%;
 			}
 		}
-		img {
+		.good-body {
+			padding: 18px;
+			.good-title {
+				font-size: 18px;
+				margin-bottom: 8px;
+				font-weight: 700;
+				color: @primary-color;
+			}
+			.good-detail {
+				margin-bottom: 10px;
+				line-height: 18px;
+				font-size: 0;
+				color: rgb(147, 153, 159);
+				.good-sell-count {
+					font-size: 14px;
+					margin-right: 12px;
+				}
+				.good-rating {
+					font-size: 14px;
+				}
+			}
+			.good-price {
+				font-weight: 700;
+				line-height: 24px;
+				.good-price-now {
+					margin-right: 8px;
+					font-size: 14px;
+					color: rgb(240, 20, 20);
+				}
+				.good-price-old {
+					text-decoration: line-through;
+					font-size: 10px;
+					color: rgb(147, 153, 159);
+				}
+			}
+		}
+		.pos() {
+			right: 18px;
+			bottom: 20px;
+		}
+		.cartcontrol-wrapper {
 			position: absolute;
-			top: 0;
-			left: 0;
-			width: 100%;
-			height: 100%;
+			.pos();
+		}
+		.good-buy {
+			@len: 16px;
+			position: absolute;
+			.pos();
+			height: @len * 2;
+			line-height: @len * 2;
+			padding: 0 @len;
+			box-sizing: border-box;
+			font-size: 12px;
+			border-radius: @len;
+			color: #fff;
+			background: @primary-color;
 		}
 	}
-	.good-body {
+	.good-info {
 		padding: 18px;
 		.good-title {
-			font-size: 18px;
-			margin-bottom: 8px;
-			font-weight: 700;
-			color: @primary-color;
+			line-height: 14px;
+			margin-bottom: 6px;
+			font-size: 16px;
+			color: rgb(7, 17, 27);
 		}
-		.good-detail {
-			margin-bottom: 10px;
-			line-height: 18px;
-			font-size: 0;
-			color: rgb(147, 153, 159);
-			.good-sell-count {
-				font-size: 14px;
-				margin-right: 12px;
-			}
-			.good-rating {
-				font-size: 14px;
-			}
-		}
-		.good-price {
-			font-weight: 700;
+		.good-text {
 			line-height: 24px;
-			.good-price-now {
-				margin-right: 8px;
-				font-size: 14px;
-				color: rgb(240, 20, 20);
-			}
-			.good-price-old {
-				text-decoration: line-through;
-				font-size: 10px;
-				color: rgb(147, 153, 159);
-			}
+			padding: 0 8px;
+			font-size: 13px;
 		}
-	}
-	.pos() {
-		right: 18px;
-		bottom: 20px;
-	}
-	.cartcontrol-wrapper {
-		position: absolute;
-		.pos();
-	}
-	.good-buy {
-		@len: 16px;
-		position: absolute;
-		.pos();
-		height: @len * 2;
-		line-height: @len * 2;
-		padding: 0 @len;
-		box-sizing: border-box;
-		font-size: 12px;
-		border-radius: @len;
-		color: #fff;
-		background: @primary-color;
 	}
 }
 </style>
---
+++ src/components/Split/Split.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="split">
+		<div class="split-header" />
+		<div class="split-body" />
+	</div>
+</template>
+
+<script>
+import Back from '@/components/Back/Back.vue'
+
+export default {
+	name: 'split',
+	data() {
+		return {}
+	},
+}
+</script>
+
+<style lang="less">
+@import '../../../static/css/variable';
+.split {
+	width: 100%;
+	height: 16px;
+	.border();
+	.split-header {
+		height: 1px;
+		.border();
+	}
+	.split-body {
+		height: 14px;
+		background: @bg-color;
+	}
+}
+</style>
